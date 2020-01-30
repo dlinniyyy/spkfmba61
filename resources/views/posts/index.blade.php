e48868cf44a1be2c1a51bb355e5b68c4a02bf3b1
Michail Babaev <dlinniyyy@yandex.ru>
2020-01-30 12:26:55 +0300

    create repo

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci amet commodi consectetur eaque est excepturi expedita, id illo impedit nulla, pariatur quam quos sapiente sequi, sit temporibus veritatis voluptas!
+@endsection
+@section('footer')
+
+@endsection
